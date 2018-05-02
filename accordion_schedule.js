011f25c4fe8e38f4867cd6ac3cc5f10a58f1155a
Yuk Tung Liu <yuktungliu@gmail.com>
2018-05-01 22:37:23 -0500

    initial commit

---
+++ accordion_schedule.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var i;
+var acc = document.getElementsByClassName("accordion");
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  }
+      acc[i].click();
+}
---
+++ navigation_menu.js
@@ -0,0 +1,33 @@
+
+// BEGIN: Common Header with Navigation Bar
+document.write("<header style='text-align:center;'>");
+
+document.write("<a href='http://www.illinois.edu' target='_blank'>");
+document.write("<img border=0 width=468 height=20 id='_x0000_i1025' ");
+document.write("src='uiorgnbluebar.gif'");
+document.write("alt='University of Illinois at Urbana-Champaign' />");
+document.write("</a>");
+document.write("<a href='http://www.stat.illinois.edu' target='_blank'>");
+document.write("<h2 style='color:blue;'> Department of Statistics</h2>");
+document.write("</a>");
+document.write("<h2>STAT 390EF: R Independent Study (Archive)</h2>");
+
+document.write("</header>");
+
+document.write("<div id='navigation' style='text-align:center;'>");
+document.write("<ul>");
+
+document.write("<li> <a href='index.html'>");
+document.write("Home</a></li>");
+document.write("<li> <a href='lessons.html'>");
+document.write("Lessons</a></li>");
+document.write("<li> <a href='CourseFAQ.html'>");
+document.write("FAQs</a></li>");
+document.write("<li><a href='LonCapaExercises.html'>LON-CAPA Exercises</a></li>")
+
+document.write("</ul>");
+//document.write("</div>");
+document.write("</div>");
+
+document.write("<br /><br />");
+//END: Common Header with Navigation Bar ");
