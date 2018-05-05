b48e341911b03ce9ebc5a42a2a9276e7c1538321
Yuk Tung Liu <yuktungliu@gmail.com>
2018-05-05 09:57:17 -0500

    Stat department homepage link updated

--- navigation_menu.js
+++ navigation_menu.js
@@ -2,12 +2,12 @@
 // BEGIN: Common Header with Navigation Bar
 document.write("<header style='text-align:center;'>");
 
-document.write("<a href='http://www.illinois.edu' target='_blank'>");
+document.write("<a href='https://stat.illinois.edu/' target='_blank'>");
 document.write("<img border=0 width=468 height=20 id='_x0000_i1025' ");
 document.write("src='uiorgnbluebar.gif'");
 document.write("alt='University of Illinois at Urbana-Champaign' />");
 document.write("</a>");
-document.write("<a href='http://www.stat.illinois.edu' target='_blank'>");
+document.write("<a href='https://stat.illinois.edu/' target='_blank'>");
 document.write("<h2 style='color:blue;'> Department of Statistics</h2>");
 document.write("</a>");
 document.write("<h2>STAT 390EF: R Independent Study (Archive)</h2>");
